c9fdc706e6daa9e90557d22617b12ea5a76a65b4
stephiedv <stephanie.dlvj@gmail.com>
2020-05-26 18:13:02 -0500

    Manipulate DOM with Queryselector

--- app.js
+++ app.js
@@ -7,4 +7,25 @@ GAME RULES:
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
-*/
+*/ 
+
+// var scorePlayer1 = 0;
+// var scorePlayer2 = 0;
+
+//VARIABLES
+var scorePlayer = [0,0];
+var roundScore = 0;
+var activePlayer = 1;
+var dice = Math.floor(Math.random() * 6)+1;
+// console.log(dice);
+
+//DOM MANIPULATION
+//SET For id="current-0" */
+document.querySelector('#current-' + activePlayer).textContent = dice; 
+//READ/GET for id="score-0"
+var readDom = document.querySelector('#score-0').textContent;
+//Changing CSS for an element Ex: class="dice"
+document.querySelector('.dice').style.display = 'none';
+
+
+
