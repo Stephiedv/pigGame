d787bc918814ea27d6ebc63acd428be773fd300f
stephiedv <stephanie.dlvj@gmail.com>
2020-05-26 19:57:38 -0500

    Events

--- app.js
+++ app.js
@@ -9,23 +9,44 @@ GAME RULES:
 
 */ 
 
-// var scorePlayer1 = 0;
-// var scorePlayer2 = 0;
-
 //VARIABLES
 var scorePlayer = [0,0];
 var roundScore = 0;
 var activePlayer = 1;
-var dice = Math.floor(Math.random() * 6)+1;
-// console.log(dice);
 
+/*
 //DOM MANIPULATION
-//SET For id="current-0" */
+//SET For id="current-0" 
 document.querySelector('#current-' + activePlayer).textContent = dice; 
 //READ/GET for id="score-0"
 var readDom = document.querySelector('#score-0').textContent;
 //Changing CSS for an element Ex: class="dice"
-document.querySelector('.dice').style.display = 'none';
+*/
+
+//document.querySelector('.dice').style.display = 'none';
+var diceDOM = document.querySelector('.dice');
+diceDOM.style.display = 'none';
+
+// METHOD GET ELEMENT BY ID
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
 
+/**** Set up an event handler ****/
+//1.Select the button where we will roll the dice
+document.querySelector('.btn-roll').addEventListener('click',function() {
+    // What happens as soon as someone clicks the button?:
+
+    //1. We need a random number
+    var dice = Math.floor(Math.random() * 6)+1;
+
+    //2. Display the result
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+    //3. Update the round score only if the roll number isn't 1
+    } 
+  );
 
